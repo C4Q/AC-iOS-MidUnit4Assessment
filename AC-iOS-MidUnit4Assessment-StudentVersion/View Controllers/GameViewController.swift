607f4b61265c91c549cb6b2762c2a88a1f0a6326
Melissa He <melissahe@ac.c4q.nyc>
2017-12-22 10:52:35 -0500

    Set up storyboard and auto layout

---
+++ AC-iOS-MidUnit4Assessment-StudentVersion/View Controllers/GameViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AC-iOS-MidUnit4Assessment-StudentVersion
+//
+//  Created by C4Q  on 12/21/17.
+//  Copyright © 2017 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
