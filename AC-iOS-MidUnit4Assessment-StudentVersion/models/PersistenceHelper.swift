11dca31a2b4a4bed06e0214b7c78c2833ff374ee
Iram Fattah <iramfattah94@gmail.com>
2017-12-23 02:37:06 -0500

    need to work on bugs and images

--- AC-iOS-MidUnit4Assessment-StudentVersion/models/PersistenceHelper.swift
+++ AC-iOS-MidUnit4Assessment-StudentVersion/models/PersistenceHelper.swift
@@ -8,66 +8,62 @@
 import  UIKit
 class DataPersistence {
     
-    // Singleton
+  
     private init() {}
     static let manager = DataPersistence()
     
     let filePath = "HandHistory.plist"
     
     struct Hand: Codable {
-        //let target: Int
+        
         let handTotal: Int
         let cards: [Card]
     }
     
-    // Save hands everytime it changes
+   
     private var playedHands = [Hand]() {
         didSet {
             saveHands()
         }
     }
     
-    // Gets the doc dir path
+  
     func documentsDirectory() -> URL {
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         return paths[0]
     }
     
-    // Gets the file path in doc dir
+   
     private func dataFilePath(withPathName path: String) -> URL {
         return DataPersistence.manager.documentsDirectory().appendingPathComponent(path)
     }
     
-    // Loads the hands into this object
+
     func loadHands() {
         var data = Data()
         do {
             data = try Data.init(contentsOf: DataPersistence.manager.dataFilePath(withPathName: filePath))
         } catch {
-            print("Error retrieving data. \(error.localizedDescription)")
+            print( "\(error.localizedDescription)")
             return
         }
         
         do {
             playedHands = try PropertyListDecoder().decode([Hand].self, from: data)
         } catch {
-            print("Plist decoding error. \(error.localizedDescription)")
+            print("\(error.localizedDescription)")
         }
     }
     
-    // Returns this object's array of hands
-    func getHands() -> [Hand] {
-        return playedHands
-    }
     
-    // Saves current array of hands into a plist into the doc dir
+    
     private func saveHands() {
         var data = Data()
         
         do {
             data = try PropertyListEncoder().encode(playedHands)
         } catch {
-            print("Plist encoding error. \(error.localizedDescription)")
+            print("\(error.localizedDescription)")
             return
         }
         
@@ -75,21 +71,19 @@ class DataPersistence {
             try data.write(to: DataPersistence.manager.dataFilePath(withPathName: filePath), options: .atomic)
             
         } catch {
-            print("Writing to disk error. \(error.localizedDescription)")
+            print(" \(error.localizedDescription)")
         }
         
     }
-    
-    // Appends an object to hands array
-    // Also saves the image in the doc dir
+    func getHands() -> [Hand] {
+        return playedHands
+    }
+  
     func addHand(playedCards: [Card],  handTotal: Int) {
         let hand = Hand(handTotal: handTotal, cards: playedCards)
         playedHands.append(hand)
     }
     
-    // Deletes saved hands
-    func deleteHands() {
-        playedHands.removeAll()
-    }
+    
     
 }
