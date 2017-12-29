6574dd40f646f36515202a3e81e22a2d48867eff
Nicole Souvenir <github@nicolesouvenir.com>
2017-12-28 20:51:16 -0500

    alerts and actions added for win, lose andstop conditions

--- AC-iOS-MidUnit4Assessment-StudentVersion/View Controller/GameViewController.swift
+++ AC-iOS-MidUnit4Assessment-StudentVersion/View Controller/GameViewController.swift
@@ -8,8 +8,6 @@
 
 /*
  TODO:
- Have an additonal card show up in the collection view as I type on one card
- Create alerts
  Saving of images using File Manager
  Saving of values using user defaults
  Add the nibs
@@ -19,7 +17,7 @@ import UIKit
 class GameViewController: UIViewController {
     
     @IBOutlet weak var gameCollectionView: UICollectionView!
-    @IBOutlet weak var cardValueLabel: UILabel!
+    @IBOutlet weak var handValueLabel: UILabel!
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var stopGameBtn: UIButton!
     @IBOutlet weak var drawCardBtn: UIButton!
@@ -28,62 +26,49 @@ class GameViewController: UIViewController {
     let cellSpacing: CGFloat = 20.0 //creating constant for the cell spacing
     
     //MARK: - What's powering the app
-//    var cardArray: [CardInfo] = [] //Empty array to add cards into as you draw them
     var cards = [CardInfo](){
         didSet{
-//            //TODO: - based on whether the total value of the 6 cards is above or below 30, an alert will pop up
-//            if cards[0].value < String(30){
-//                //MARK: - GameOver alert: https://www.youtube.com/watch?v=lDMV8Um_8n8
-//                //gameOverAlert(titleText: "GameOver", message: "You were \(30 - Int(cards[0].value)!) away from 30")
-//                print("Not quite to 30!")
-//            }
-//
-//            if cards[0].value > String(30){
-//                //MARK: - BUST alert
-//                //gameOverAlert(titleText: "BUST", message: "You went over 30")
-//                print("You went over 30")
-//            }
-            
-//            for card in cards {
-//                cards.append(card)
-//            }
-//
-            //change the value based on the card that is added
-            cardValueLabel.text = "Current Hand Value: \(cards[0].value)"
             self.gameCollectionView.reloadData()
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        cardValueLabel.text = "Current Hand Value: 0"
+        handValueLabel.text = "Current Hand Value: 0"
     }
     
     //MARK: - Button actions for the game
     @IBAction func DrawCardButtonPressed(_ sender: UIButton) {
-        //Call Card API to get one new card
+        //Get one card
         loadCard()
-        //adding a card to the array everytime user draws a card
     }
     
     @IBAction func stopGameButtonPressed(_ sender: UIButton) {
-        //ResetGame: Current Value: 0,no cards in collection view
-        cardValueLabel.text = "Current Hand Value: 0"
+//        //ResetGame: Current Value: 0, no cards in collection view
+//        cards = []
+//        handValueLabel.text = "Current Hand Value: 0"
+        stopGameAlert()
     }
-    
-    @IBAction func newGameButtonPressed(_ sender: UIButton) {
-        //Call the cardDeck API to get a new deck of cards
-        //getNewDeck()
-    }
-    
-    
     
     //MARK: - loading in cards from the internet
     func loadCard(){
         //set completion
         let setOnlineCardImage: ([CardInfo]) -> Void = {(onlineCard: [CardInfo]) in
-            self.cards.append(onlineCard.first!) //appending the first card to the array of CardInfo
-            print("You have cards!")
+            self.cards.append(onlineCard.first!) //instead of setting onlineCard to the array (self.cards = onlineCard), APPEND the onlineCard into the array
+            print("card is appended to array")
+            //update the score
+            let cardValue = GameModel.cardValues[onlineCard[0].value] //string that goes into the dict to get the value that is an Int
+            GameModel.currentPoints += (cardValue ?? 0)
+            
+            //update label
+            self.handValueLabel.text = "Current Hand Value: \(GameModel.currentPoints)"
+            
+            //win situation
+            if GameModel.currentPoints == GameModel.pointsToWin{
+                self.winGameAlert()
+            } else if GameModel.currentPoints > GameModel.pointsToWin{
+                self.loseGameAlert()
+            }
         }
         CardAPIClient.manager.getOneNewCard(from: setOnlineCardImage,
                                             errorHandler: {print($0)})
@@ -100,33 +85,90 @@ class GameViewController: UIViewController {
     //        CardAPIClient.manager.getNewCardDeck(from: setOnlineCardDeck,
     //                                             errorHandler: {print($0)})
     //    }
+    
+    //MARK: - creating functions for the alerts
+    func winGameAlert(){
+        // win alert
+        let alertController = UIAlertController(title: "You win!",
+                                                message:"#Boss",
+                                                preferredStyle: UIAlertControllerStyle.alert)
+        //create alert action
+        let newGameAction = UIAlertAction(title: "New Game", style: UIAlertActionStyle.default) {
+            UIAlertAction in
+            //call the cardDeck API to get a new deck of cards
+            self.cards = []
+            self.handValueLabel.text = "Current Hand Value: 0"
+            GameModel.currentPoints = 0
+            print("button pressed")
+        }
+        //add the actions
+        alertController.addAction(newGameAction)
+        //present alert controller
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func loseGameAlert(){
+        // lose alert
+        let alertController = UIAlertController(title: "Defeat!",
+                                                message:"You went over 30 points!",
+                                                preferredStyle: UIAlertControllerStyle.alert)
+        //create alert action
+        let newGameAction = UIAlertAction(title: "New Game", style: UIAlertActionStyle.default) {
+            UIAlertAction in
+            //call the cardDeck API to get a new deck of cards
+            self.cards = []
+            self.handValueLabel.text = "Current Hand Value: 0"
+            GameModel.currentPoints = 0
+            print("button pressed")
+        }
+        //add the actions
+        alertController.addAction(newGameAction)
+        //present alert controller
+        self.present(alertController, animated: true, completion: nil)
+    }
+    func stopGameAlert(){
+        // stop game alert
+        let alertController = UIAlertController(title: "So Soon?",
+                                                message:"You were \(GameModel.pointsToWin - GameModel.currentPoints) points away from 30",
+                                                preferredStyle: UIAlertControllerStyle.alert)
+        //create alert action
+        let newGameAction = UIAlertAction(title: "New Game", style: UIAlertActionStyle.default) {
+            UIAlertAction in
+            //call the cardDeck API to get a new deck of cards
+            self.cards = []
+            self.handValueLabel.text = "Current Hand Value: 0"
+            GameModel.currentPoints = 0
+            print("button pressed")
+        }
+        //add the actions
+        alertController.addAction(newGameAction)
+        //present alert controller
+        self.present(alertController, animated: true, completion: nil)
+    }
 }
+
+
 
 //MARK: - CollectionView of books by Category
 extension GameViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        var cardArray: [CardInfo] = [] //Empty array to add cards into as you draw them
-        
-        for card in cards {
-            cardArray.append(card)
-            print("adding card to cardArray")
-        }
-        
-        return cardArray.count
+        return cards.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let card = cards[indexPath.row]
-        
+        let card = cards[indexPath.row] //hand of card
+        print(card.value)
+        print(card)
         guard let cardCell = gameCollectionView.dequeueReusableCell(withReuseIdentifier: "Game Collection View Cell", for: indexPath) as? GameCollectionViewCell else {return UICollectionViewCell()}
         
         //cardCell properties
-        cardCell.cardValueLabel.text = card.value
+        cardCell.cardValueLabel.text = "\(GameModel.cardValues[card.value] ?? 0)"
         cardCell.cardImage.image = nil
         
         //MARK: - Getting and setting card images
         let imageUrlStr = card.images.png //ONLY "IF LET" IF IT'S AN OPTIONAL!
+        
         
         //set completion
         let setImageToOnlineImage : (UIImage) -> Void = {(onlineImage: UIImage) in
@@ -146,13 +188,15 @@ extension GameViewController: UICollectionViewDelegate, UICollectionViewDataSour
     }
 }
 
+
+
 //MARK: Manipulating CollectionView cellsizing
 extension GameViewController: UICollectionViewDelegateFlowLayout {
     
     //returns how large the cells should be
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let numCells: CGFloat = 2//number of cells visible in row
-        let numSpace: CGFloat = numCells + 5
+        let numSpace: CGFloat = numCells + 7
         let screenWidth = UIScreen.main.bounds.width  // screen width fo the device
         //print("cells are large enough!")
         //cgsize expects a tuple: (width, height)
@@ -172,6 +216,7 @@ extension GameViewController: UICollectionViewDelegateFlowLayout {
     }
 }
 
-//MARK: - crating functions for the alerts
-func gameOverAlert(){}
-func bustAlert(){}
+
+
+
+
