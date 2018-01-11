f9e28f782e38f05ff35bfa76a34a3b0a96caa1e4
Masai Young <masai.young@gmail.com>
2018-01-11 10:15:32 -0500

    Maybe now?

---
+++ AC-iOS-MidUnit4Assessment-StudentVersion/CardCollectionViewCell/CardCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CardCollectionViewCell.swift
+//  AC-iOS-MidUnit4Assessment-StudentVersion
+//
+//  Created by Masai Young on 12/22/17.
+//  Copyright © 2017 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cardImage: UIImageView!
+    @IBOutlet weak var bottomLabel: UILabel!
+    
+//    required init?(coder aDecoder: NSCoder) { //Storyboard init
+//        super.init(coder: aDecoder)
+//        if let view = Bundle.main.loadNibNamed("CardCollectionViewCell", owner: self, options: nil)?.first as? UIView {
+//            addSubview(view)
+//            view.frame = self.bounds
+//        }
+//    }
+}
